b2e1a873876fa2da8e92f7fb6fedacd3a2886034
Shelton Suen <sheltonsuen@gmail.com>
2020-05-05 18:22:16 +0800

    process double decrease when over `SellIn`

--- src/__tests__/updateGildedRose.test.js
+++ src/__tests__/updateGildedRose.test.js
@@ -1,11 +1,14 @@
+import MockDate from 'mockdate';
+
 import updateGildedRose from '../updateGildedRose';
 
 const MOCK_GOODS = [
   {
+
     name: 'Fake Good1',
     productionDate: '2019-01-01',
-    sellIn: 60,
-    quality: 0,
+    sellIn: 8,
+    quality: 10,
   },
   {
     name: 'Fake Good2',
@@ -15,7 +18,25 @@ const MOCK_GOODS = [
   },
 ];
 
+beforeEach(() => {
+  MockDate.set(new Date('2019-01-10'));
+});
+
+afterEach(() => {
+  MockDate.reset();
+});
+
 test('should return same number of goods', () => {
   expect(updateGildedRose(MOCK_GOODS))
     .toHaveLength(2);
+});
+
+test('should double decrease quality when over production date', () => {
+  expect(updateGildedRose(MOCK_GOODS)[0].quality)
+    .toBe(8);
+
+  MockDate.set(new Date('2019-01-11'));
+
+  expect(updateGildedRose(MOCK_GOODS)[0].quality)
+    .toBe(6);
 });
--- src/updateGildedRose.js
+++ src/updateGildedRose.js
@@ -1,3 +1,15 @@
+import dayjs from 'dayjs';
+
 export default function (goods) {
-  return goods;
+  return goods.map(v => {
+    const expirationDate = dayjs(v.productionDate)
+      .add(v.sellIn, 'day');
+
+    const qualityDowngrade = dayjs()
+      .diff(expirationDate, 'day') * 2;
+    return {
+      ...v,
+      quality: v.quality - qualityDowngrade
+    };
+  });
 }
